36cc016d7dedd68f37d442ee1e0f853acd08b249
Kim Jayoon <beginin15@gmail.com>
2020-02-12 23:21:53 +0900

    feat : enum Type의 toString() 오버라이딩, 파싱 메소드 정의
    * toString() : 해당 Type의 정보(타입명, 스피드, 확률)를 출력하기 위해 오버라이딩
    
    * parseType() : 인자로 전달받은 문자열과 동일한 타입을 반환하는 메소드

--- src/main/java/TYPE.java
+++ src/main/java/TYPE.java
@@ -7,7 +7,7 @@ public enum Type {
     private int speed;
     private int randomLimit;
 
-    Type(String typeName, int speed, int randomLimit) {
+    private Type(String typeName, int speed, int randomLimit) {
         this.typeName = typeName;
         this.speed = speed;
         this.randomLimit = randomLimit;
@@ -24,4 +24,23 @@ public enum Type {
     public int getRandomLimit() {
         return randomLimit;
     }
+
+    @Override
+    public String toString() {
+        int forwardPercentage = (Monster.RANDOM_MAX - randomLimit) * 10;
+        return String.format("[ %s 타입 / 속도 : %s / 이동 확률 : %s ]", typeName, speed, forwardPercentage);
+    }
+
+    public static Type parseType(String typeName) {
+        switch (typeName) {
+            case "노말" :
+                return Type.NORMAL;
+            case "비행" :
+                return Type.FLYING;
+            case "에스퍼" :
+                return Type.PSYCHIC;
+        }
+        return null;
+    }
 }
+
