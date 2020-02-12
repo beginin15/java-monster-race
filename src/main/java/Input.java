bd9257371eefa129545ac7546cb85b746191d251
Kim Jayoon <beginin15@gmail.com>
2020-02-12 23:46:07 +0900

    refactor, feat : Input의 모든 멤버를 정적으로 변경, 요구사항 변경에 따른 몬스터 데이터 입력부 변경
    * Input 클래스를 유틸 클래스처럼 모든 멤버를 정적으로 변경함. Scanner 객체가 멤버 변수이므로 상태를 가지고 있다고 생각해서 인스턴스로 생성했었지만 Scanner도 정적 멤버으로 변경.
    
    * 외부에서의 인스턴스 생성을 막기 위해 생성자를 private으로 선언.
    
    * selectNumberOfMenu() : 메뉴 번호 1~n에 해당되는 범위의 숫자를 입력받는 메소드.
    
    * getIntOfSpecificRange() : min ~ max 범위의 숫자를 입력받고, 범위에 벗어나는 경우 다시 입력받도록 재귀적으로 호출. (min보다 커야 하고, max랑 같거나 작아야 함)
    
    * getNewMonster() : step3에서는 정해진 수만큼 몬스터 정보를 입력받고 생성했다면, step4는 몬스터 생성 메뉴를 선택할 때마다 1마리씩 입력 및 생성하도록 요구 사항이 변경됨. 그에 대응하기 위한 메소드.
    
    * getExitCode() : 몬스터 리스트를 화면에 출력 및 정지된 상태를 유지하기 위해 키보드 입력을 받는 메소드. 입력받은 값은 아무런 의미 없음.
    
    * getNewProfile() : 몬스터의 정보에서 새롭게 변경될 값들을 입력받는 메소드.
    
    * 커밋을 분할했어야 했는데 깜빡해서 한번에 커밋.

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,40 +1,63 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Input {
 
-    private Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
-    public int getCountOfMonsters() {
-        System.out.println("출전할 몬스터는 몇 마리?");
-        return getPositiveInt();
+    private Input() { }
+
+    public static int selectNumberOfMenu(int menuRange) {
+        String message = String.format("원하는 메뉴를 선택해 주세요(1 - %d)", menuRange);
+        System.out.println(message);
+        return getIntOfSpecificRange(0, menuRange);
     }
 
-    public List<Monster> getParticipants(int countOfMonsters) {
-        System.out.println("경주할 몬스터 이름과 종류를 입력하세요. (쉼표(,)를 기준으로 구분)");
-        List<Monster> monsters = new ArrayList<>();
-        while (monsters.size() != countOfMonsters) {
-            try {
-                monsters.add(createMonster());
-            } catch (InvalidDelimiterException | UnknownMonsterTypeException e) {
-                System.out.println(e.getMessage());
-            }
+    public static void getExitCode() {
+        System.out.println("종료하려면 아무 키 + Enter를 입력해주세요.");
+        scanner.nextLine();
+    }
+
+    public static Monster getNewMonster() {
+        System.out.println("몬스터 이름과 종류를 입력하세요. (쉼표(,)를 기준으로 구분)");
+        Monster monster = null;
+        try {
+            monster = createMonster();
+        } catch (InvalidDelimiterException | UnknownMonsterTypeException e) {
+            System.out.println(e.getMessage());
         }
-        return monsters;
+        return monster;
     }
 
-    public int getRounds() {
+    public static String[] getNewProfile() {
+        System.out.println("새로운 이름과 종류를 입력하세요. (쉼표(,)를 기준으로 구분)");
+        return getProfileOfMonster();
+    }
+
+    public static int getRounds() {
         System.out.println("몇 라운드?");
         return getPositiveInt();
     }
 
-    public void closeScanner() {
+    public static void closeScanner() {
         if (scanner != null)
             scanner.close();
     }
 
-    private int getPositiveInt() {
+    // 파라미터 : min (미포함), max (포함)
+    private static int getIntOfSpecificRange(int min, int max) {
+        int input;
+        try {
+            input = Integer.parseInt(scanner.nextLine());
+            if (input <= min || input > max)
+                throw new InvalidRangeException("유효 범위에서 벗어난 입력값입니다.");
+        } catch (NumberFormatException | InvalidRangeException e) {
+            System.out.println(e.getMessage());
+            input = getIntOfSpecificRange(min, max);
+        }
+        return input;
+    }
+
+    private static int getPositiveInt() {
         int input;
         try {
             input = Integer.parseInt(scanner.nextLine());
@@ -47,22 +70,22 @@ public class Input {
         return input;
     }
 
-    private Monster createMonster() throws InvalidDelimiterException, UnknownMonsterTypeException {
+    private static Monster createMonster() throws InvalidDelimiterException, UnknownMonsterTypeException {
         final int indexOfName = 0;
         final int indexOfType = 1;
 
-        String[] monsterInfo = getMonsterInfo();
-        if (monsterInfo.length <= indexOfType)
+        String[] monsterProfile = getProfileOfMonster();
+        if (monsterProfile.length <= indexOfType)
             throw new InvalidDelimiterException("잘못된 구분자입니다.");
 
-        Monster monster = MonsterFactory.getMonsterInstance(monsterInfo[indexOfName], monsterInfo[indexOfType].trim());
+        Monster monster = MonsterFactory.getMonsterInstance(monsterProfile[indexOfName], monsterProfile[indexOfType]);
         if (monster == null)
             throw new UnknownMonsterTypeException("존재하지 않는 몬스터 타입입니다.");
         return monster;
     }
 
-    private String[] getMonsterInfo() {
-        String input = scanner.nextLine().trim();
+    private static String[] getProfileOfMonster() {
+        String input = scanner.nextLine().replaceAll(" ", "");
         return input.split(",");
     }
 
