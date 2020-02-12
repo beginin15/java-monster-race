14ea59eeaf17310e66a9a39264ff244afb4e11dd
Kim Jayoon <beginin15@gmail.com>
2020-02-12 23:27:24 +0900

    refactor : main 실행 시, 메인 메뉴(MainMenu)를 실행하도록 변경
    * 게임의 메인 메뉴에 해당되는 MainMenu 객체를 생성 및 실행

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -1,9 +1,7 @@
 public class GameMachine {
 
     public static void main(String[] args) {
-        MonsterRace race = new MonsterRace();
-        race.ready();
-        race.start();
-        race.finish();
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.execute();
     }
 }
