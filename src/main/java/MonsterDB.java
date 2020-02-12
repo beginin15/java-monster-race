cac5607e4ff7e14cd18d24b481db8146ee3d0a8f
Kim Jayoon <beginin15@gmail.com>
2020-02-13 00:09:33 +0900

    feat : 몬스터 정보를 관리하는 MonsterDB 클래스 정의
    * 몬스터 DB 테이블을 표현.
    
    * 몬스터 정보는 ProfileMenu와 MonsterRace 두 메뉴에게 모두 필요한 데이터이므로 MonsterDB 객체를 함께 참조하도록 구현할 예정.
    
    * 해당 클래스의 메소드들은 테이블의 데이터를 추가, 수정, 삭제하는 sql문처럼 표현. (잘 된건지는 모르겠음)
    
    * 데이터 수정 혹은 삭제 시, 인자로 전달 받은 comparatorMonster는 기존 리스트에 존재하지 않는 새로운 주소를 가지고 있지만 Monster 클래스의 논리적 동등함을 주소값 비교가 아닌 이름과 타입으로 구현했기 때문에 comparatorMonster와 같은 객체가 존재하는지 비교 수행하도록 함. (해당 방식은 수정, 삭제될 때 기존 리스트에서 객체를 가져오는 것이 아니라 비교자가 되는 새로운 객체를 생성하는 방식이므로 자연스럽지 못한 것 같아서 고민됨)

---
+++ src/main/java/MonsterDB.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonsterDB {
+
+    private List<Monster> monsters = new ArrayList<>();
+
+    public void showMonsterList() {
+        if (monsters.size() == 0) {
+            System.out.println("-- 비 어 있 음 --");
+            return;
+        }
+        for (Monster monster : monsters) {
+            System.out.println(monster.getProfile());
+        }
+    }
+
+    public boolean modifyMonster(Monster comparatorMonster, String newName, Type newType) {
+        int index = monsters.indexOf(comparatorMonster);
+        if(index == -1)
+            return false;
+        comparatorMonster.setMonsterName(newName);
+        comparatorMonster.setType(newType);
+        monsters.set(index, comparatorMonster);
+        return true;
+    }
+
+    public boolean addMonster(Monster newMonster) {
+        if (monsters.contains(newMonster))
+            return false;
+        monsters.add(newMonster);
+        return true;
+    }
+
+    public boolean deleteMonster(Monster comparatorMonster) {
+        return monsters.remove(comparatorMonster);
+    }
+}
