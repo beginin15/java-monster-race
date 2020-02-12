3753d1b29af77d18c04d9cdc1fb89de6533d2699
Kim Jayoon <beginin15@gmail.com>
2020-02-12 23:20:03 +0900

    feat : MenuContainer 클래스 정의
    * 여러 개의 서브 메뉴를 관리하는 클래스
    
    * MenuContainer도 하나의 메뉴이므로  MenuItem 인터페이스를 구현
    
    * isExit : 이전 메뉴로 이동할 때 사용. 현재 실행중 인 메뉴 컨테이너의 execute()를 종료하기 위한 조건. 
    
    * menuItems : 여러 개의 서브 메뉴를 list로 관리
    
    * execute() : 입력받은 번호에 해당되는 서브 메뉴 객체의 execute를 실행
    
    * addSubMenu() : 인자로 전달받은 메뉴 객체를 서브 메뉴 list에 추가. 생성자에서 필요한 서브 메뉴를 세팅할 때 사용하는 메소드.
    
    * showMenus() : 서브 메뉴 list를 출력
    
    * selectMenu() : 사용자에게 메뉴 번호를 입력받아 그에 해당되는 서브 메뉴 객체를 반환하는 메소드

---
+++ src/main/java/MenuContainer.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuContainer implements MenuItem {
+
+    protected boolean isExit = false;
+    private List<MenuItem> menuItems = new ArrayList<>();
+
+    @Override
+    public void execute() {
+        do {
+            showMenus();
+            MenuItem selectedMenu = selectMenu();
+            selectedMenu.execute();
+        } while (!isExit);
+        isExit = false;
+    }
+
+    protected void addSubMenu(MenuItem subMenu) {
+        menuItems.add(subMenu);
+    }
+
+    private void showMenus() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            String menu = String.format("%d. %s", i + 1, menuItems.get(i));
+            System.out.println(menu);
+        }
+    }
+
+    private MenuItem selectMenu() {
+        int numberOfMenu = Input.selectNumberOfMenu(menuItems.size());
+        return menuItems.get(numberOfMenu - 1);
+    }
+}
